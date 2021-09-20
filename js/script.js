8dea08153562185a0e371c551a0b3ae46b8ccefc
Miroslavetsh <dfgshte@gmail.com>
2021-09-20 23:28:14 +0300

    Update 2021-09-20T20:28:10.347Z

--- js/script.js
+++ js/script.js
@@ -378,17 +378,21 @@ const ModalsInterface = (function () {
 
 ModalsInterface.init()
 
-const giftBoxes = document.querySelector('.giftbox__packages-inner')
-const giftBoxPreview = document.querySelector('.giftbox__open')
-function loadGiftBox(event) {
-  if (event.target.classList.contains('giftbox__packages')) {
-    const boxImg = event.target.dataset.img
-    giftBoxPreview.style.background = `url("${boxImg}") center center no-repeat`
-    giftBoxPreview.style.backgroundSize = 'contain'
-    console.log(event.target)
+try {
+  const giftBoxes = document.querySelector('.giftbox__packages-inner')
+  const giftBoxPreview = document.querySelector('.giftbox__open')
+  function loadGiftBox(event) {
+    if (event.target.classList.contains('giftbox__packages')) {
+      const boxImg = event.target.dataset.img
+      giftBoxPreview.style.background = `url("${boxImg}") center center no-repeat`
+      giftBoxPreview.style.backgroundSize = 'contain'
+      console.log(event.target)
+    }
   }
+  giftBoxes.addEventListener('click', loadGiftBox)
+} catch (err) {
+  console.error(err)
 }
-giftBoxes.addEventListener('click', loadGiftBox)
 
 
 /* === Homepage === */
