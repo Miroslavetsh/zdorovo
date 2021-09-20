7be3ae737912586f0c3de3308a549362488a046c
Miroslavetsh <dfgshte@gmail.com>
2021-09-20 23:27:10 +0300

    Fix giftbox module

--- src/js/giftbox.js
+++ src/js/giftbox.js
@@ -1,11 +1,15 @@
-const giftBoxes = document.querySelector('.giftbox__packages-inner')
-const giftBoxPreview = document.querySelector('.giftbox__open')
-function loadGiftBox(event) {
-  if (event.target.classList.contains('giftbox__packages')) {
-    const boxImg = event.target.dataset.img
-    giftBoxPreview.style.background = `url("${boxImg}") center center no-repeat`
-    giftBoxPreview.style.backgroundSize = 'contain'
-    console.log(event.target)
+try {
+  const giftBoxes = document.querySelector('.giftbox__packages-inner')
+  const giftBoxPreview = document.querySelector('.giftbox__open')
+  function loadGiftBox(event) {
+    if (event.target.classList.contains('giftbox__packages')) {
+      const boxImg = event.target.dataset.img
+      giftBoxPreview.style.background = `url("${boxImg}") center center no-repeat`
+      giftBoxPreview.style.backgroundSize = 'contain'
+      console.log(event.target)
+    }
   }
+  giftBoxes.addEventListener('click', loadGiftBox)
+} catch (err) {
+  console.error(err)
 }
-giftBoxes.addEventListener('click', loadGiftBox)
