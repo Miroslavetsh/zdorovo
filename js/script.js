cb8b85182a44e70213d5a30017894a2cf8593ba5
Miroslavetsh <dfgshte@gmail.com>
2021-10-07 11:50:43 +0300

    Update 2021-10-07T08:50:40.064Z

--- js/script.js
+++ js/script.js
@@ -256,118 +256,121 @@ try {
   })
 } catch {}
 
-// Modal
-const ModalsInterface = (function () {
-  const searchEveryModal = () => {
-    return Array.from(document.querySelectorAll('.modal'))
-  }
-
-  /**
-   * @param {Array | NodeList} buttonsToOpen
-   */
-
-  class Modal {
-    constructor({
-      modalElement,
-      timeout,
-      buttonsToOpen,
-      isStoreModalClosedState,
-      isModalVideo,
-    }) {
-      this.modal = modalElement
-      this.timeout = timeout // Number
-
-      this.openButtons = buttonsToOpen
-      this.storeModalState = isStoreModalClosedState // Boolean
-      this.isModalVideo = isModalVideo
-      this.frame = this.modal.querySelector('iframe')
+try {
+  // Modal
+  const ModalsInterface = (function () {
+    const searchEveryModal = () => {
+      return Array.from(document.querySelectorAll('.modal'))
     }
 
-    open() {
-      this.modal.classList.add('_opened')
+    /**
+     * @param {Array | NodeList} buttonsToOpen
+     */
 
-      if (this.isModalVideo) {
-        this.frame.src = this.frame.dataset.src
-      }
-    }
+    class Modal {
+      constructor({
+        modalElement,
+        timeout,
+        buttonsToOpen,
+        isStoreModalClosedState,
+        isModalVideo,
+      }) {
+        this.modal = modalElement
+        this.timeout = timeout // Number
 
-    openWithTimeout() {
-      if (this.timeout) {
-        setTimeout(() => {
-          this.modal.classList.add('_opened')
-        }, this.timeout)
-      }
-    }
-
-    _close() {
-      if (this.storeModalState) {
-        localStorage.setItem(
-          `modalWindow_${this.modal.dataset['modalName']}`,
-          'closedAndHidden'
-        )
+        this.openButtons = buttonsToOpen
+        this.storeModalState = isStoreModalClosedState // Boolean
+        this.isModalVideo = isModalVideo
+        this.frame = this.modal.querySelector('iframe')
       }
 
-      if (this.isModalVideo) {
-        this.frame.src = ''
+      open() {
+        this.modal.classList.add('_opened')
+
+        if (this.isModalVideo) {
+          this.frame.src = this.frame.dataset.src
+        }
       }
 
-      this.modal.classList.remove('_opened')
-    }
+      openWithTimeout() {
+        if (this.timeout) {
+          setTimeout(() => {
+            this.modal.classList.add('_opened')
+          }, this.timeout)
+        }
+      }
 
-    setClosingFunction() {
-      this.modal.addEventListener('click', (event) => {
-        event.preventDefault()
-        if (event.target.dataset['close'] !== undefined) this._close()
-      })
-    }
-
-    setOpeningFunction() {
-      this.openButtons.forEach((button) => {
-        button.addEventListener('click', () => {
-          this.open()
-        })
-      })
-    }
-  }
-
-  return {
-    init: function () {
-      const modalsArray = searchEveryModal()
-      modalsArray.forEach((modal) => {
-        const buttonsToOpen = document.querySelectorAll(
-          `[data-modal-open="${modal.dataset['modalName']}"]`
-        )
-        // Auto open modal if timeout
-        const timeout = Number(modal.dataset['timeout']) ?? 0
-        const isStoreModalClosedState =
-          modal.dataset['storeState'] === 'true' ?? false
-        const isModalVideo = modal.querySelector('iframe') ? true : false
-        const modalWindow = new Modal({
-          modalElement: modal,
-          timeout: timeout,
-          buttonsToOpen: buttonsToOpen,
-          isStoreModalClosedState: isStoreModalClosedState,
-          isModalVideo: isModalVideo,
-        })
-
-        if (
-          localStorage.getItem(`modalWindow_${modal.dataset['modalName']}`) !==
-          'closedAndHidden'
-        ) {
-          if (modalWindow.timeout) modalWindow.openWithTimeout()
+      _close() {
+        if (this.storeModalState) {
+          localStorage.setItem(
+            `modalWindow_${this.modal.dataset['modalName']}`,
+            'closedAndHidden'
+          )
         }
 
-        // If we have a buttons need to close modal
+        if (this.isModalVideo) {
+          this.frame.src = ''
+        }
 
-        if (buttonsToOpen.length) modalWindow.setOpeningFunction()
+        this.modal.classList.remove('_opened')
+      }
 
-        modalWindow.setClosingFunction()
-      })
-    },
-  }
-})()
+      setClosingFunction() {
+        this.modal.addEventListener('click', (event) => {
+          event.preventDefault()
+          if (event.target.dataset['close'] !== undefined) this._close()
+        })
+      }
 
-ModalsInterface.init()
+      setOpeningFunction() {
+        this.openButtons.forEach((button) => {
+          button.addEventListener('click', () => {
+            this.open()
+          })
+        })
+      }
+    }
+
+    return {
+      init: function () {
+        const modalsArray = searchEveryModal()
+        modalsArray.forEach((modal) => {
+          const buttonsToOpen = document.querySelectorAll(
+            `[data-modal-open="${modal.dataset['modalName']}"]`
+          )
+          // Auto open modal if timeout
+          const timeout = Number(modal.dataset['timeout']) ?? 0
+          const isStoreModalClosedState =
+            modal.dataset['storeState'] === 'true' ?? false
+          const isModalVideo = modal.querySelector('iframe') ? true : false
+          const modalWindow = new Modal({
+            modalElement: modal,
+            timeout: timeout,
+            buttonsToOpen: buttonsToOpen,
+            isStoreModalClosedState: isStoreModalClosedState,
+            isModalVideo: isModalVideo,
+          })
+
+          if (
+            localStorage.getItem(
+              `modalWindow_${modal.dataset['modalName']}`
+            ) !== 'closedAndHidden'
+          ) {
+            if (modalWindow.timeout) modalWindow.openWithTimeout()
+          }
+
+          // If we have a buttons need to close modal
+
+          if (buttonsToOpen.length) modalWindow.setOpeningFunction()
+
+          modalWindow.setClosingFunction()
+        })
+      },
+    }
+  })()
+
+  ModalsInterface.init()
+} catch {}
 
 try {
   const giftBoxes = document.querySelector('.giftbox__packages-inner')
@@ -698,7 +701,13 @@ try {
 
 try {
   function getFullDate() {
-    return new Date().toISOString().split('T')[0].replaceAll('-', '.').split('.').reverse().join('.')
+    return new Date()
+      .toISOString()
+      .split('T')[0]
+      .replace(/-/g, '.')
+      .split('.')
+      .reverse()
+      .join('.')
   }
 
   class Images {
@@ -737,22 +746,29 @@ try {
       let timing = images.length
 
       setTimeout(() => {
-        rouletteItems.forEach((rouletteItem) => {
+        rouletteItems.forEach((rouletteItem, index) => {
           rouletteItem.classList.toggle('_hidden')
         })
       }, timing * 1000)
 
       let intervalHandler = () => {
+        if (counter === timing) {
+          clearInterval(interval)
+        }
+
         counter++
 
-        dateBlock.innerHTML = images[counter].text
-        let img = document.createElement('img')
-        img.src = images.find((img) => img.id === counter).src
-        image.innerHTML = ''
-        image.append(img)
+        
+        if (images[counter] && images[counter].text) {
+          dateBlock.innerHTML = images[counter].text
+          let img = document.createElement('img')
+          img.src = images.find((img) => img.id === counter).src
+          image.innerHTML = ''
+          image.append(img)
+        }
       }
 
-      setInterval(intervalHandler, 1000)
+      const interval = setInterval(intervalHandler, 1000)
 
       button.classList.add('_animated')
     },
